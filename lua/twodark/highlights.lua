b548fdc0e9b4caa565497ce889ac5c466c854e18
Jyn Fairchild <jpfairchild73@gmail.com>
2024-05-03 23:38:57 -0700

    trial and error

--- lua/twodark/highlights.lua
+++ lua/twodark/highlights.lua
@@ -104,7 +104,7 @@ hl.syntax = {
     Float = colors.LOREM,
     Boolean = colors.LOREM,
     Type = colors.LOREM,
-    Structure = c.lua_brackets_and_true_statemets, -- lua "{ }, true, 300"
+    Structure = {fg = c.lua_brackets_and_true_statemets},  -- lua "{ }, true, 300"
     StorageClass = colors.IPSM,
     Identifier = {fg = c.LOREM, fmt = cfg.code_style.variables},
     Constant = colors.LOREM,
