f0f70f68fa8eea6341d1d5dd7f581af10a9b04d6
Jyn Fairchild <jpfairchild73@gmail.com>
2024-05-05 00:34:35 -0700

    trial and error

--- lua/twodark/highlights.lua
+++ lua/twodark/highlights.lua
@@ -41,7 +41,7 @@ local colors = {
 }
 
 hl.common = {
-    Normal = {fg = c.func, bg = cfg.transparent and c.none or c.bg0},
+    Normal = {fg = c.keyword, bg = cfg.transparent and c.none or c.bg0},
     Terminal = {fg = c.fg, bg = cfg.transparent and c.none or c.bg0},
     EndOfBuffer = {fg = cfg.ending_tildes and c.bg2 or c.bg0, bg = cfg.transparent and c.none or c.bg0},
     FoldColumn = {fg = c.fg, bg = cfg.transparent and c.none or c.bg1},  -- indent_blankline
