6d469afe337b6a9022da515c7d1568f3b4010b70
Jyn Fairchild <jpfairchild73@gmail.com>
2024-05-04 17:05:50 -0700

    trial and error

--- lua/twodark/palette.lua
+++ lua/twodark/palette.lua
@@ -74,7 +74,7 @@ return {
         warning = "#C48E4F",
         type = "#5DA994",  -- Path, class names
         type_builtin = "#B8A965", -- KeyError, type(str, bool)
-        variable = "#5CAD8A",  -- params
+        variable = "#7FA37F",  -- params
         variable_builtin = "#C99076",  -- self.
         variable_member = "#C99076",  -- unknown
         variable_parameter = "#DBD7CA",  -- builtin params
