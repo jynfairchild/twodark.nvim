e421f059ea47144cb7be3553e9005007b5345592
Jyn Fairchild <jpfairchild73@gmail.com>
2024-05-04 22:22:09 -0700

    trial and error

--- lua/twodark/palette.lua
+++ lua/twodark/palette.lua
@@ -61,7 +61,6 @@ return {
         module = "#FF0000",  -- unknown
         namespace = "#CCCCCC",
         note = "#CCCCCC",
-        none = "#7D7D7D",
         number = "#E8C262",
         number_float = "#E8C262",
         operator = "#6D795E",  -- = < > |
