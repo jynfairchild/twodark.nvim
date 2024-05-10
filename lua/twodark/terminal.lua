2a192ee273b2d3b35baa630d182fdc092df63ac3
Jyn Fairchild <jpfairchild73@gmail.com>
2024-05-10 00:17:34 -0700

    trial and error

--- lua/twodark/terminal.lua
+++ lua/twodark/terminal.lua
@@ -8,17 +8,17 @@ function M.setup()
     vim.g.terminal_color_1 = c.terminal_typing  -- typing
     vim.g.terminal_color_2 = c.terminal_keyword  -- keyword
     vim.g.terminal_color_3 = c.text_secondary
-    vim.g.terminal_color_4 = c.test_teal
+    vim.g.terminal_color_4 = c.terminal_directory  -- current directory
     vim.g.terminal_color_5 = c.terminal_carrot  -- > carrot
     vim.g.terminal_color_6 = c.terminal_directory  -- directories
     vim.g.terminal_color_7 = c.test_orange
-    vim.g.terminal_color_8 = c.test_yellow
+    vim.g.terminal_color_8 = c.terminal_cursor  -- terminal cursor
     vim.g.terminal_color_9 = c.test_green
     vim.g.terminal_color_10 = c.placeholder
     vim.g.terminal_color_11 = c.placeholder
     vim.g.terminal_color_12 = c.blue
     vim.g.terminal_color_13 = c.purple
-    vim.g.terminal_color_14 = c.test_teal
+    vim.g.terminal_color_14 = c.test_blue
     vim.g.terminal_color_15 = c.fg
 end
 
