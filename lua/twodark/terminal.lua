d9bf7fe4e7617806cf071bbbcf32ae78c514ae69
Jyn Fairchild <jpfairchild73@gmail.com>
2024-05-10 00:01:10 -0700

    trial and error

--- lua/twodark/terminal.lua
+++ lua/twodark/terminal.lua
@@ -6,19 +6,19 @@ function M.setup()
     if not cfg.term_colors then return end
     vim.g.terminal_color_0 = c.black
     vim.g.terminal_color_1 = c.red
-    vim.g.terminal_color_2 = c.test_yellow  -- maybe keyword
-    vim.g.terminal_color_3 = c.test_blue  -- maybe unfinished
+    vim.g.terminal_color_2 = c.terminal_keyword  -- keyword
+    vim.g.terminal_color_3 = c.placeholder
     vim.g.terminal_color_4 = c.blue
     vim.g.terminal_color_5 = c.terminal_carrot  -- > carrot
-    vim.g.terminal_color_6 = c.cyan
+    vim.g.terminal_color_6 = c.test_orange  -- maybe mid type
     vim.g.terminal_color_7 = c.fg
     vim.g.terminal_color_8 = c.grey
     vim.g.terminal_color_9 = c.red
-    vim.g.terminal_color_10 = c.test_teal -- maybe keyword
-    vim.g.terminal_color_11 = c.test_orange -- maybe unfinished
+    vim.g.terminal_color_10 = c.placeholder
+    vim.g.terminal_color_11 = c.placeholder
     vim.g.terminal_color_12 = c.blue
     vim.g.terminal_color_13 = c.purple
-    vim.g.terminal_color_14 = c.cyan
+    vim.g.terminal_color_14 = c.test_teal
     vim.g.terminal_color_15 = c.fg
 end
 
